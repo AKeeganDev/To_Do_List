ffceac14c31f80482b9f9304aab3b420f65ce40f
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-03-23 12:41:34 -0400

    Setup final layout for list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 import './style.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 function component() {
   const element = document.createElement('div');
