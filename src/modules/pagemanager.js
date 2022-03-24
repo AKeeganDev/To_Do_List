5f6a0cbc5845f004b2e02e569763392dde9d36cb
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-03-24 19:00:32 -0400

    add remove-completed functionality AND savestate of completion for tasks

--- src/modules/pagemanager.js
+++ src/modules/pagemanager.js
@@ -19,14 +19,34 @@ export default class PageManager {
 
   updateButtons;
 
+  clearButton = document.querySelector('.clear-button');
+
   roundArrow = '<svg id="turn" class="svg-inline--fa fa-share" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="share" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M503.7 226.2l-176 151.1c-15.38 13.3-39.69 2.545-39.69-18.16V272.1C132.9 274.3 66.06 312.8 111.4 457.8c5.031 16.09-14.41 28.56-28.06 18.62C39.59 444.6 0 383.8 0 322.3c0-152.2 127.4-184.4 288-186.3V56.02c0-20.67 24.28-31.46 39.69-18.16l176 151.1C514.8 199.4 514.8 216.6 503.7 226.2z"></path></svg><!-- <i id="turn" class="fa-solid fa-share"></i> Font Awesome fontawesome.com -->';
 
   trashCan = '<svg class="svg-inline--fa fa-trash-can" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-can" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"></path></svg><!-- <i class="fa-solid fa-trash-can"></i> Font Awesome fontawesome.com -->';
+
+  updateCompletion = (parentIndex) => {
+    // takes the parent index of a task and matches it to the correct index in the task Array
+    for (let i = 0; i < this.taskList.length; i += 1) {
+      if (this.taskList[i].index === parentIndex + 1) {
+        if (this.taskList[i].completed === true) {
+          this.taskList[i].completed = false;
+          this.pushTasksToStorage();
+          return;
+        }
+        if (this.taskList[i].completed === false) {
+          this.taskList[i].completed = true;
+          this.pushTasksToStorage();
+        }
+      }
+    }
+  }
 
   loadCheckBoxListeners = () => {
     this.checkBoxes.forEach((checkbox) => {
       checkbox.addEventListener('click', (e) => {
         e.target.classList.toggle('checked');
+        this.updateCompletion(parseInt(e.target.parentElement.parentElement.id, 10));
       });
     });
   }
@@ -105,8 +125,19 @@ export default class PageManager {
     Object.values(tasksObj).forEach((task) => {
       const index = this.taskList.length + 1;
       const oldTask = new Task(task.description);
+      oldTask.completed = task.completed;
       oldTask.index = index;
       this.taskList.push(oldTask);
+    });
+  }
+
+  loadClearButtonListener = () => {
+    this.clearButton.addEventListener('click', () => {
+      const incompletedTasks = this.taskList.filter((task) => task.completed === true);
+      for (let i = 0; i < incompletedTasks.length; i += 1) {
+        const arrayIndex = incompletedTasks[i].index - 1;
+        this.removeTask(arrayIndex);
+      }
     });
   }
 
@@ -150,5 +181,7 @@ export default class PageManager {
         this.addTask();
       }
     });
+
+    this.loadClearButtonListener();
   };
 }
--- src/modules/task.js
+++ src/modules/task.js
@@ -5,13 +5,24 @@ export default class Task {
     this.description = description;
   }
 
-  toHTML = () => `<label for="Item${this.index}">
+  toHTML = () => {
+    if (this.completed === true) {
+      return `<label for="Item${this.index}">
+      <button type="button" class="check-box checked">&check;</button>
+      <input class="task" type="text" value="${this.description}" name="Item${this.index}" id="${this.index}">
+    </label>
+    <div id="index${this.index}" class="update-button">
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    </div>`;
+    }
+    return `<label for="Item${this.index}">
       <button type="button" class="check-box">&check;</button>
       <input class="task" type="text" value="${this.description}" name="Item${this.index}" id="${this.index}">
     </label>
     <div id="index${this.index}" class="update-button">
     <i class="fa-solid fa-ellipsis-vertical"></i>
     </div>`;
+  }
 
   static toTrashCan = () => '<svg class="svg-inline--fa fa-trash-can" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-can" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"></path></svg><!-- <i class="fa-solid fa-trash-can"></i> Font Awesome fontawesome.com -->';
 
